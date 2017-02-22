8c7c64101e8cb58407de5c884f4a3da0770e4b50
rghossi <rafael.ghossi@gmail.com>
2017-02-22 16:18:46 -0300

    configured table view cells

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Rafael Rodrigues Ghossi on 2/22/17.
+//  Copyright © 2017 Rafael Rodrigues Ghossi. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
